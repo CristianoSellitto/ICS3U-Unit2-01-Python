aff20557b14f3ce28efb74890cad3e4a59ad7171
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-09-21 14:47:12 +0000

    Fixed typo

--- circle_area.py
+++ circle_area.py
@@ -10,7 +10,7 @@ import math
 def main():
     # Finds the area and perimeter of a circle with a radius of 15mm
 
-    print("A circle is has a radius of 15mm")
+    print("A circle has a radius of 15mm")
     print("\nThis circle's area is {}mm²".format(math.pi * 15**2))
     print("This circle's perimeter is {}mm".format(math.pi * (15 * 2)))
 
